b042bf4f965d9b10b36a4a073c2cc9fceb8123ab
CJ <CJ@Lenovo-PC>
2017-04-04 11:41:27 -0400

    initial project

---
+++ src/TestClass.java
@@ -0,0 +1,13 @@
+/*
+ * I hereby decalre upon my word of honor that I have neither
+ * given nor received unauthorized help on this work.
+ * -Collin Johnson
+ */
+
+/**
+ *
+ * @author CJ
+ */
+public class TestClass {
+    /**project 3
+}
